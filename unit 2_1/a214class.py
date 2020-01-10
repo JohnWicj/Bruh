f574d9428bcb6346e4df0e94c7e2eed2ac092457
Tashon Washington <washingt05@Student.psdr3.org>
2020-01-10 13:19:03 -0600

    changes to become cool

---
+++ unit 2_1/a214class.py
@@ -0,0 +1,10 @@
+import tkinter as tk
+
+root = tk.Tk()
+root.geometry("400x300")
+
+user_label = tk.label(root, text = "useenaame")
+user_button = tk.Button
+user_label.pack()
+
+root.mainloop()
---
+++ unit 2_1/activity214.py
@@ -0,0 +1,13 @@
+##############################################################################
+#   a113_TR_simple_window1.py
+#   Example soulution: Change its size to 200 by 100 pixels.
+##############################################################################
+import tkinter as tk
+
+# main window
+root = tk.Tk()
+root.wm_geometry("100x100")
+frame_login.grid
+frame_login = tk.Frame(root)
+root.mainloop()
+
